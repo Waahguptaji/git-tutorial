50a0b94c9fdad09d2c24c44915f117230df2a61f
sahilgupta <sahilgupta742@gmail.com>
2023-04-30 00:33:23 +0530

    Version 1

---
+++ Loop_Pattern7.java
@@ -0,0 +1,47 @@
+/*
+   *
+  ***
+ *****
+  ***
+   *
+
+
+
+
+*/
+package com.company;
+
+import java.util.Scanner;
+
+public class Loop_Pattern7 {
+    public static void main(String[] args) {
+        Scanner myobj = new Scanner(System.in);
+        System.out.println("ENter the number n: ");
+        int n = myobj.nextInt();
+        int i, j, k;
+        for ( i = 1; i <=n ; i++) {
+            for (j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+
+            }
+            for (k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+
+            for ( i = n -1 ; i >= 1  ; i--) {
+                for ( j = 1; j<= n- i  ; j++) {
+                    System.out.print(" ");
+
+                }
+                for ( k = 1 ; k <= 2*i -1 ; k++) {
+                    System.out.print("*");
+
+                }
+            System.out.println();
+        }
+    }
+}
+
---
+++ chnge.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class chnge {
+    public static void main(String[] args) {
+        System.out.println("HEllo I am learnig git");
+        System.out.println("WE can edit the previous commit without creating a new commit by putting --amend at last while committing");
+    }
+}
